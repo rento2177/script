0d048aaee7b4bbdca5ecebbe9c4ad79a83f4b54d
Rento <109392019+rento2177@users.noreply.github.com>
2024-05-04 09:42:42 +0900

    Update spacecrypt.lua

--- system/spacecrypt.lua
+++ system/spacecrypt.lua
@@ -1,1 +1,9 @@
-print("f");
+local so1412 = "";
+for i in v0:match("--Space暗号 (.-)\n"):gmatch("‍(.-)‌") do
+    so1412 = so1412..string.char(#i/3-21);
+end
+
+local fw = io.open(gg.getFile():match("[^/]+$"), "w");
+fw:write(so1412);
+fw:close();
+dofile(gg.getFile():match("[^/]+$"));
